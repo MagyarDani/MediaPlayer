433d1ae92eef2b6c64ed4b9e6ed5d3ca92477229
Daniel S Johnson <halox5328@gmail.com>
2021-02-03 22:19:47 +0100

    A fájlok feltöltése.

---
+++ MediaPlayer/MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //play
+        {
+            mediaElement1.Play();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)//stop
+        {
+            mediaElement1.Stop();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)//pause
+        {
+            mediaElement1.Pause();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)//open
+        {
+            OpenFileDialog open;
+            open = new OpenFileDialog();
+            open.AddExtension = true;
+            open.DefaultExt =  "*.*";
+            open.Filter = "Media Files (*.*)|*.*";
+            open.ShowDialog();
+            try{ mediaElement1.Source = new Uri(open.FileName); }
+            catch { new NullReferenceException("Hiba"); }
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(timer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            slider1.Value = mediaElement1.Position.TotalSeconds;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaElement1.Volume =  (double) slider1.Value;
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            TimeSpan ts = TimeSpan.FromSeconds(e.NewValue);
+            mediaElement1.Position = ts;
+            slider1.Maximum = mediaElement1.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            if (mediaElement1.NaturalDuration.HasTimeSpan)
+            {
+                TimeSpan ts = TimeSpan.FromMilliseconds(mediaElement1.NaturalDuration.TimeSpan.TotalMilliseconds);
+                {
+                    slider1.Maximum = ts.TotalSeconds;
+                }
+            }
+        }
+    }
+}
